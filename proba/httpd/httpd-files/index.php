96603a61b23b65ca3ae779f938f7fd30683f9655
Ákos Orosz <lekvar4thewin@gmail.com>
2023-07-24 06:46:15 +0200

    init

---
+++ proba/httpd/httpd-files/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Egyszerű üzenet weboldal</title>
+</head>
+<body>
+  <?php
+  $servername = "database";
+  $username = "wordpress";
+  $password = "wordpress";
+  $dbname = "messages";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Hiba a kapcsolódáskor: " . $conn->connect_error);
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = $_POST["message"];
+
+    $sql = "INSERT INTO messages (message) VALUES ('$message')";
+    if ($conn->query($sql) === TRUE) {
+      echo "Az üzenet sikeresen hozzáadva.";
+    } else {
+      echo "Hiba az üzenet hozzáadásakor: " . $conn->error;
+    }
+  }
+
+  $sql = "SELECT message FROM messages";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    echo "<h2>Üzenetek:</h2>";
+    while($row = $result->fetch_assoc()) {
+      echo "<p>" . $row["message"] . "</p>";
+    }
+  } else {
+    echo "Nincsenek üzenetek.";
+  }
+
+  $conn->close();
+  ?>
+
+  <h2>Új üzenet hozzáadása:</h2>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <input type="text" name="message">
+    <input type="submit" value="Küldés">
+  </form>
+</body>
+</html>
